79eb992c5b73da69f791e60531879fda7639c615
Elvis Felix <elvisjaspion@gmail.com>
2021-02-07 03:46:05 -0300

    Atualizando server.js

--- src/server.js
+++ src/server.js
@@ -17,4 +17,7 @@ app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'avatar')));
 app.use(routes)
 
-app.listen(3333)
+app.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server started on port 3333!');
+  });
